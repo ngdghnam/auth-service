bb0a95754fdfca82fb8f177b48e10dda8f31c796
Nam <stghoainam4002@gmail.com>
2025-01-26 22:52:51 +0700

    Demo Authenication Routes

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,17 @@
-import { express, Router } from "express";
-
+import express from "express";
+import shortid from "shortid";
 const router = express.Router();
+import * as controller from "../controllers/auth.controller.js";
+import { authMiddleware } from "../middlewares/validate.middleware.js";
+
+// Define routes
+// Endpoint to create a new user
+router.post("/register", controller.register);
+
+router.post("/login", controller.login);
+
+router.post("/refresh-token", authMiddleware, controller.refreshToken);
+
+router.get("/users", authMiddleware, controller.getAllUsers);
 
 export default router;
